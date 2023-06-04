5c746984b983706129126f18ec352c10af0e1275
Lyrismet <ana0203tir@gmail.com>
2023-06-05 01:23:36 +0700

    feat: setup context api

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, NavLink } from "react-router-dom";
-import { SiShopware } from "react-icons/si";
-import { MdOutlineCancel } from "react-icons/md";
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {Link, NavLink} from "react-router-dom";
+import {SiShopware} from "react-icons/si";
+import {MdOutlineCancel} from "react-icons/md";
+import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 
 import {useStateContext} from '../contexts/ContextProvider';
 
-import { links } from '../data/dummy';
+import {links} from '../data/dummy';
 
 
 const Sidebar = () => {
@@ -17,28 +17,31 @@ const Sidebar = () => {
     return (
         <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
             {activeMenu && (<>
-                <div className="flex justify-between items-center">
-                    <Link to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-x1 font-extrabold tracking-tight dark:text-white text-slate-900">
-                        <SiShopware className="text-3x1" /> <span>Shopper's Hub</span>
-                    </Link>
-                    <TooltipComponent content="Menu" position="BottomCenter">
-                        <button type="button" onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} className="text-x1 rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
-                            <MdOutlineCancel />
-                        </button>
-                    </TooltipComponent>
-                </div>
+                    <div className="flex justify-between items-center">
+                        <Link to="/" onClick={() => setActiveMenu(false)}
+                              className="items-center gap-3 ml-3 mt-4 flex text-x1 font-extrabold tracking-tight dark:text-white text-slate-900">
+                            <SiShopware className="text-3x1"/> <span>Shopper's Hub</span>
+                        </Link>
+                        <TooltipComponent content="Menu" position="BottomCenter">
+                            <button type="button" onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+                                    className="text-x1 rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+                                <MdOutlineCancel/>
+                            </button>
+                        </TooltipComponent>
+                    </div>
                     <div className="mt-10">
                         {links.map((item) => (
                             <div key={item.title}>
                                 <p className="text-gray-400 m-3 mt-4 uppercase">
                                     {item.title}
                                 </p>
-                                {item.links.map((link) =>(
+                                {item.links.map((link) => (
                                     <NavLink
-                                    to = {`/${link.name}`}
-                                    key = {link.name}
-                                    onClick = {()=>{}}
-                                    className = {({isActive})=> isActive ? activeLink : normalLink}>
+                                        to={`/${link.name}`}
+                                        key={link.name}
+                                        onClick={() => {
+                                        }}
+                                        className={({isActive}) => isActive ? activeLink : normalLink}>
                                         {link.icon}
                                         <span className="capitalize">
                                             {link.name}
