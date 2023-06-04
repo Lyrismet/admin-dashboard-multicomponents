9a609dc80478bed9eda443d1f76e100e8e203134
Lyrismet <ana0203tir@gmail.com>
2023-06-05 01:10:16 +0700

    feat: add context to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 import './index.css';
+import {ContextProvider} from "./contexts/ContextProvider";
 
 ReactDOM.render(
-    <App />,document.getElementById('root')
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root')
 )
