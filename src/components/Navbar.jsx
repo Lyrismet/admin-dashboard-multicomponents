802ca99b161ae88c2d5a390fa5523d7814900649
Lyrismet <ana0203tir@gmail.com>
2023-06-05 17:06:34 +0700

    feat: added base navButton func

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,19 @@ import avatar from '../data/avatar.jpg'
 import { Cart, Chat, Notification, UserProfile} from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 
+const NavButton = ({title, customFunc, icon, color, dotColor}) => (
+    <TooltipComponent content={title}
+    position="BottomCenter">
+        <button></button>
+
+    </TooltipComponent>
+)
+
 const Navbar = () => {
+    const { activeMenu, setActiveMenu} = useStateContext();
+
     return (
-        <div>
+        <div className="flex justify-between p-2 md:mx-6 relative">
             
         </div>
     );
