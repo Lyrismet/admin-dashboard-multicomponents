88c750ea075384362dced4be89b2da810ed7504b
Lyrismet <ana0203tir@gmail.com>
2023-06-09 22:42:32 +0700

    feat: icon resize

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,12 +25,12 @@ const Sidebar = () => {
             {activeMenu && (<>
                     <div className="flex justify-between items-center">
                         <Link to="/" onClick={handleCloseSideBar}
-                              className="items-center gap-3 ml-3 mt-4 flex text-x1 font-extrabold tracking-tight dark:text-white text-slate-900">
+                              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
                             <SiCashapp className="text-3x1"/> <span>Shopper's Hub</span>
                         </Link>
                         <TooltipComponent content="Menu" position="BottomCenter">
                             <button type="button" onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-                                    className="text-x1 rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+                                    className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
                                 <MdOutlineCancel/>
                             </button>
                         </TooltipComponent>
