22b60a9381471ddd6cf0bed04c566b3136c96ab6
Lyrismet <ana0203tir@gmail.com>
2023-06-09 21:58:48 +0700

    feat: add handleCloseSidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,6 +11,12 @@ import {links} from '../data/dummy';
 
 const Sidebar = () => {
     const {activeMenu, setActiveMenu, screenSize, setScreenSize} = useStateContext();
+    const handleCloseSideBar = () => {
+        if(activeMenu && screenSize <= 900) {
+            setActiveMenu(false);
+        }
+    }
+
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
@@ -18,7 +24,7 @@ const Sidebar = () => {
         <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
             {activeMenu && (<>
                     <div className="flex justify-between items-center">
-                        <Link to="/" onClick={() => setActiveMenu(false)}
+                        <Link to="/" onClick={handleCloseSideBar}
                               className="items-center gap-3 ml-3 mt-4 flex text-x1 font-extrabold tracking-tight dark:text-white text-slate-900">
                             <SiCashapp className="text-3x1"/> <span>Shopper's Hub</span>
                         </Link>
