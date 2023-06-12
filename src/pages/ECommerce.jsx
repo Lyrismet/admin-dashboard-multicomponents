ed778df8655e219e041b657fa7ea87d43f440194
Lyrismet <ana0203tir@gmail.com>
2023-06-13 00:03:22 +0700

    feat: added elements

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -31,7 +31,19 @@ const ECommerce = () => {
                     {earningData.map((item) => (
                         <div
                         key={item.title} className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl">
-
+                            <button type="button"
+                            style = {{ color: item.iconColor, backgroundColor: item.iconBg}}
+                            className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
+                                {item.icon}
+                            </button>
+                            <p className="mt-3">
+                                <span className="text-lg font-semibold">
+                                    {item.amount}
+                                </span>
+                                <span className={`text-sm text-${item.pcColor}`}>
+                                    {' '}{item.percentage}
+                                </span>
+                            </p>
                         </div>
                     ))}
                 </div>
