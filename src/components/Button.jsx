fbb0b15e61e67f39b5fc9c606cc392498fe446df
Lyrismet <ana0203tir@gmail.com>
2023-06-13 17:48:14 +0700

    style: formatted w prettier

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Button = ({ bgColor, color, size, text, borderRadius}) => {
-    return (
-        <button
-        type="button"
-        style={{backgroundColor: bgColor, color: color, borderRadius: borderRadius}}
-        className={`text-${size} p-3 hover:drop-shadow-xl hover:bg-light-gray text-white`}
-        >
-            {text}
-        </button>
-    );
-}
-export default Button
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      style={{
+        backgroundColor: bgColor,
+        color: color,
+        borderRadius: borderRadius,
+      }}
+      className={`text-${size} p-3 hover:drop-shadow-xl hover:bg-light-gray text-white`}
+    >
+      {text}
+    </button>
+  );
+};
+export default Button;
