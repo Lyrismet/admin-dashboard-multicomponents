b555f46a907dc8bcba930cb53967c4347104417a
Lyrismet <ana0203tir@gmail.com>
2023-06-14 21:06:19 +0700

    feat: added recent trans-s

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -144,6 +144,18 @@ const ECommerce = () => {
             </div>
           </div>
         </div>
+
+        <div className="flex gap-10 m-4 flex-wrap justify-center">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
+            <div className="flex justify-between items-center gap-2">
+              <p className="text-xl font-semibold">Recent Transactions</p>
+              <DropDown currentMode={currentMode} />
+            </div>
+          </div>
+        </div>
+
+
+
       </div>
 
   );
