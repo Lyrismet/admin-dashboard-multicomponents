ebb27ebe689b4200b15332bfc51c72265c65e88a
Lyrismet <ana0203tir@gmail.com>
2023-06-14 21:12:24 +0700

    feat: added imports

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -8,6 +8,10 @@ import {
   earningData,
   SparklineAreaData,
   ecomPieChartData,
+  recentTransactions,
+    weeklyStats,
+    dropdownData,
+    medicalproBranding
 } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 import product9 from "../data/product9.jpg";
@@ -152,6 +156,29 @@ const ECommerce = () => {
             <div className="flex justify-between items-center gap-2">
               <p className="text-xl font-semibold">Recent Transactions</p>
               <DropDown currentMode={currentMode} />
+            </div>
+            <div className="mt-10 w-72 md:w-400">
+              {recentTransactions.map((item) => (
+                  <div key={item.title} className="flex justify-between mt-4">
+                    <div className="flex gap-4">
+                      <button
+                          type="button"
+                          style={{
+                            color: item.iconColor,
+                            backgroundColor: item.iconBg,
+                          }}
+                          className="text-2xl rounded-lg p-4 hover:drop-shadow-xl"
+                      >
+                        {item.icon}
+                      </button>
+                      <div>
+                        <p className="text-md font-semibold">{item.title}</p>
+                        <p className="text-sm text-gray-400">{item.desc}</p>
+                      </div>
+                    </div>
+                    <p className={`text-${item.pcColor}`}>{item.amount}</p>
+                  </div>
+              ))}
             </div>
           </div>
         </div>
