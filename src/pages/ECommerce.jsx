dc586bd2ee75d793f6597483a2585820294cebff
Lyrismet <ana0203tir@gmail.com>
2023-06-14 21:24:24 +0700

    feat: added weekly stats func

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -217,8 +217,51 @@ const ECommerce = () => {
               </button>
             </div>
             <div className="mt-10 ">
+              {weeklyStats.map((item) => (
+                  <div key={item.title} className="flex justify-between mt-4 w-full">
+                    <div className="flex gap-4">
+                      <button
+                          type="button"
+                          style={{ background: item.iconBg }}
+                          className="text-2xl hover:drop-shadow-xl text-white rounded-full p-3"
+                      >
+                        {item.icon}
+                      </button>
+                      <div>
+                        <p className="text-md font-semibold">{item.title}</p>
+                        <p className="text-sm text-gray-400">{item.desc}</p>
+                      </div>
+                    </div>
 
+                    <p className={`text-${item.pcColor}`}>{item.amount}</p>
+                  </div>
+              ))}
+              <div className="mt-4">
+                <SparkLine currentColor={currentColor} id="area-sparkLine" height="160px" type="Area" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+              </div>
             </div>
+
+          </div>
+          <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+            <div className="flex justify-between">
+              <p className="text-xl font-semibold">MedicalPro Branding</p>
+              <button type="button" className="text-xl font-semibold text-gray-400">
+                <IoIosMore />
+              </button>
+            </div>
+            <p className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-24 bg-orange-400 py-0.5 px-2 text-gray-200 mt-10">
+              16 APR, 2021
+            </p>
+
+            <div className="flex gap-4 border-b-1 border-color mt-6">
+              {medicalproBranding.data.map((item) => (
+                  <div key={item.title} className="border-r-1 border-color pr-4 pb-2">
+                    <p className="text-xs text-gray-400">{item.title}</p>
+                    <p className="text-sm">{item.desc}</p>
+                  </div>
+              ))}
+            </div>
+
           </div>
         </div>
 
