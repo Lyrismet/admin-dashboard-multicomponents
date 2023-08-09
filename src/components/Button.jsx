c66c922c9c7729c6c93a2e6ed319cb0317070d9d
lyrismet <ana0203tir@gmail.com>
2023-08-09 13:42:53 +0300

    ref: contained component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { useStateContext } from "../contexts/ContextProvider";
 
-const Button = ({ bgColor, color, size, text, borderRadius }) => {
-  return (
-    <button
-      type="button"
-      style={{
-        backgroundColor: bgColor,
-        color: color,
-        borderRadius: borderRadius,
-      }}
-      className={`text-${size} p-3 hover:drop-shadow-xl hover:bg-light-gray text-white`}
-    >
-      {text}
-    </button>
-  );
+const Button = ({bgColor, color, size, text, borderRadius}) => {
+    const {setIsClicked, initialState} = useStateContext();
+    return (
+        <button
+            type="button"
+            onClick={() => setIsClicked(initialState)}
+            style={{
+                backgroundColor: bgColor,
+                color: color,
+                borderRadius: borderRadius,
+            }}
+            className={`text-${size} p-3 hover:drop-shadow-xl hover:bg-light-gray text-white`}
+        >
+            {text}
+        </button>
+    );
 };
 export default Button;
